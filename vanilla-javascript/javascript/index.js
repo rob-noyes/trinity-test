3babec36eae8e019ba6afe40dbe6d3cc0a009113
Rob Noyes <robertjnoyes1@gmail.com>
2022-03-04 11:49:39 -0800

    image slider complete desktop and mobile

--- vanilla-javascript/javascript/index.js
+++ vanilla-javascript/javascript/index.js
@@ -6,6 +6,11 @@ const two = document.querySelector('#two');
 const three = document.querySelector('#three');
 const four = document.querySelector('#four');
 
+const carousel = document.querySelector('.carousel-track');
+
+const back = document.querySelector('.back');
+const next = document.querySelector('.next');
+
 dots.forEach((element) => {
   element.addEventListener('click', () => {
     const notClicked = Array.from(dots).filter((notClicked) => {
@@ -18,4 +23,30 @@ dots.forEach((element) => {
 
     element.classList.add('active');
   });
+});
+
+next.addEventListener('click', () => {
+  carousel.scrollLeft += 400;
+});
+
+back.addEventListener('click', () => {
+  if (carousel.scrollLeft > 0) {
+    carousel.scrollLeft -= 400;
+  }
+});
+
+one.addEventListener('click', () => {
+  carousel.scrollLeft = 0;
+});
+
+two.addEventListener('click', () => {
+  carousel.scrollLeft = 755;
+});
+
+three.addEventListener('click', () => {
+  carousel.scrollLeft = 1515;
+});
+
+four.addEventListener('click', () => {
+  carousel.scrollLeft = 2275;
 });
